8dca2dd01f9043a6e45e5bd884fe8fffec045805
Guillaume Arp <48798757+GuillaumeArp@users.noreply.github.com>
2022-10-28 16:03:01 +0200

    updated script

--- articles.py
+++ articles.py
@@ -12,14 +12,22 @@ load_dotenv()
 KEY=os.getenv("APIKEY")
 USERNAME=os.getenv("USERNAME")
 USERPWD=os.getenv("USERPWD")
+FOLDER_PATH = '/home/guillaume/Python_Projects/DST_DE_project'
 
-def get_articles_update(filename, begin_date, end_date, clean=True):
+client = MongoClient(
+    f"mongodb+srv://{USERNAME}:{USERPWD}@nyt-de.ganwi.mongodb.net/?retryWrites=true&w=majority",
+    server_api=ServerApi('1'),
+    serverSelectionTimeoutMS=5000
+)
+
+db = client.nyt
+col = db['articles']
+
+def get_articles_update(filename, clean=True):
     """Requests articles about Covid-19 from the New York Times API, returns a list of dictionaries, and saves it to a json file.
 
     Args:
         filename (str): Name of the json file to save the articles to.
-        begin_date (str): Begin date of the articles search, in the format YYYYMMDD.
-        end_date (str): End date of the articles search, in the format YYYYMMDD.
         clean (bool, optional): Toggles the cleaning of unwanted keys. Defaults to True.
 
     Returns:
@@ -29,7 +37,7 @@ def get_articles_update(filename, begin_date, end_date, clean=True):
     results_list = []
     request_headers = {"Accept": "application/json"}
     
-    with open('/home/guillaume/Python_Projects/DST_DE_project/src/begin_date.txt', 'r') as read_begin_date:
+    with open(f'{FOLDER_PATH}/src/begin_date.txt', 'r') as read_begin_date:
         begin_date = read_begin_date.read()
         
     begin_dt = datetime.datetime.strptime(read_begin_date, '%Y%m%d')
@@ -55,7 +63,7 @@ def get_articles_update(filename, begin_date, end_date, clean=True):
     new_begin_dt = begin_dt + datetime.timedelta(days=8)
     new_begin_date = new_begin_dt.strftime('%Y%m%d')
     
-    with open('/home/guillaume/Python_Projects/DST_DE_project/src/begin_date.txt', 'w') as outfile:
+    with open(f'{FOLDER_PATH}/src/begin_date.txt', 'w') as outfile:
         outfile.write(new_begin_date)
             
     if clean:
@@ -63,26 +71,24 @@ def get_articles_update(filename, begin_date, end_date, clean=True):
         for i in lst_clean:
             i.pop('multimedia', None)
 
-        with open(f"/home/guillaume/Python_Projects/DST_DE_project/src/{filename}", 'w') as outfile:
+        with open(f"{FOLDER_PATH}/src/{filename}", 'w') as outfile:
             json.dump(lst_clean, outfile, indent=4)
 
         return lst_clean
     
     else:
-        with open(f"/home/guillaume/Python_Projects/DST_DE_project/src/{filename}", 'w') as outfile:
+        with open(f"{FOLDER_PATH}/src/{filename}", 'w') as outfile:
             json.dump(results_list, outfile, indent=4)
         
         return results_list
+    
+def upload_articles_to_mongo(articles):
+    """Uploads a list of articles to a MongoDB database.
 
-
-
-
-client = MongoClient(
-    f"mongodb+srv://{USERNAME}:{USERPWD}@nyt-de.ganwi.mongodb.net/?retryWrites=true&w=majority",
-    server_api=ServerApi('1'),
-    serverSelectionTimeoutMS=5000
-)
-
-db = client.nyt
-col = db['articles']
-# results = col.insert_many(articles_update)
+    Args:
+        articles_update (list): list of dictionaries containing articles data.
+    Returns:
+        None
+    """
+    articles = get_articles_update(f'{FOLDER_PATH}/articles_update.json')
+    col.insert_many(articles)
